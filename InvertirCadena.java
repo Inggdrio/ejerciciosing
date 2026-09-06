828888a399e030fe8a1831270d304ec79c17416d
Inggdrio <gdbolanos@estudiante.uniajc.edu.co>
2026-09-06 18:13:57 -0500

    Add files via upload

---
+++ InvertirCadena.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InvertirCadena {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un texto: ");
+        String texto = sc.nextLine();
+
+        String invertida = "";
+
+        for (int i = texto.length() - 1; i >= 0; i--) {
+            invertida += texto.charAt(i);
+        }
+
+        System.out.println("Texto invertido: " + invertida);
+
+        sc.close();
+    }
+}
